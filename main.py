40bc0255f83641a455aa340450ae38f28e90e6ab
Aren <radiationbolt@gmail.com>
2024-06-17 15:10:53 +0100

    need testing
    The script is complete all that's left is the testing phases
    it'll will be used in different environment and scenarios to fix bugs and make the code lighter, and more efficient

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 #import necessary libs
-<<<<<<< Updated upstream
-import argparse
-from genericpath import isfile
-=======
 from functools import partial
 import argparse
 from genericpath import isfile
 import sys
->>>>>>> Stashed changes
 import os
 import logging
 import itertools
-import string
+import string 
 import multiprocessing
 import hashlib
+import time
 
 ## Banners
 def banner():
@@ -27,36 +23,6 @@ default_characters = {
  'S': string.punctuation,
  '?': string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
 }
-<<<<<<< Updated upstream
-# Hashing function
-def hash_word(word, hashtype):
-    # Example using SHA256; you can use other algorithms as needed
-    hash_type = hashtype.lower() # Convert to lowercase for consistency
-    try:
-        hash_object = getattr(hashlib, hash_type)()
-    except AttributeError:
-        print(f"Invalid hash type: {hash_type}")
-    hash_object.update(word)
-    ## assignement is not needed here just return hash_object.hexdigest()
-    hash_value = hash_object.hexdigest()
-    return hash_value
-
-# Wordlist generator function
-def wordlist_generator(pattern="L?N", min_length=3, max_length=10, exclude=''):
-    for length in range(min_length, max_length + 1):
-        for word_tuple in itertools.product(*(default_characters[char] for char in pattern)):
-            word = ''.join(word_tuple)
-            if len(word) == length and not any(char in word for char in exclude):
-                yield word
-
-# Parallel hashing function
-def parallel_hashing(word_generator):
-    with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
-        # Use 'imap' for lazy iteration over the results
-        for hashed_word in pool.imap(hash_word, word_generator):
-            yield hashed_word
-            
-=======
 times = 0
 # Compare function
 # customized to suit my needs
@@ -64,110 +30,53 @@ times = 0
 def compare_hash(orig_hashs, target_hash):
     global times
     if orig_hashs[0] == target_hash:    
-            print(orig_hashs[1])
-            open("Working", "w")
-            
+           return True, orig_hashs[0], orig_hashs[1]
     else:
         times = times+1
-        print(f"Tried {times} times\n")
+        print(f"Tried {times} times")
 
 # Hashing function
 def hash_word(word, hashtype):
-    # Example using SHA256; you can use other algorithms as needed
-    hash_type = hashtype.lower() # Convert to lowercase for consistency
-    try:
-        hash_object = getattr(hashlib, hash_type)()
-    except AttributeError:
-        print(f"Invalid hash type: {hash_type}")
-    hash_object.update(word.encode('utf-8'))
-    ## assignement is not needed here just return hash_object.hexdigest()
-    hash_value = hash_object.hexdigest()
-    return hash_value, word
+       # Example using SHA256; you can use other algorithms as needed
+       hash_type = hashtype.lower() # Convert to lowercase for consistency
+       try:
+           hash_object = getattr(hashlib, hash_type)()
+       except AttributeError:
+                print(f"Invalid hash type: {hash_type}")
+       hash_object.update(word.encode('utf-8'))
+       ## assignement is not needed here just return hash_object.hexdigest()
+       hash_value = hash_object.hexdigest()
+       return hash_value, word
 
 # Wordlist generator function
 def wordlist_generator(pattern="L?N", min_length=3, max_length=10, exclude=''):
+ # Assuming default_characters is defined elsewhere
     for length in range(min_length, max_length + 1):
         for word_tuple in itertools.product(*(default_characters[char] for char in pattern)):
             word = ''.join(word_tuple)
-            if len(word) == length and not any(char in word for char in exclude):
+            if not any(char in word for char in exclude):
                 yield word
+
                 
 # Wordlist read (generator) function
 def wordlist_read(files_list):
-    for i in files_list:
-        for words in open(i, "r").readlines():
-            yield words.strip()
+    for is_true, item in files_list:
+        if not is_true:
+        # Handle the case where the boolean is True
+            for words in open(item, "r").readlines():
+                    yield words.strip()    
             
 # Parallel hashing function
 def parallel_hashing(word_generator, hashtype):
     partial_hash_word = partial(hash_word, hashtype=hashtype)
     with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+
         # Use 'imap' for lazy iteration over the results
         for hashed_word, word in pool.imap(partial_hash_word,word_generator):
+            if hashed_word is None:
+                break # Stop the loop if the stop event is set
             yield hashed_word, word
-    
->>>>>>> Stashed changes
-## Analyse input and check errors
-def analyse(args):
-    print(args)
-    ## Crack Mode
-    ## direct-hashtype-hashed-wordlist(default) or direct-hashtype-hashed-wordlist-output
-<<<<<<< Updated upstream
-    if args.mode == "direct":
-        if args.type != None:
-            if args.hashed != None:
-                args.hashed = analyse_hashed(args.hashed)
-                if args.wordlist != None:
-                    args.wordlist = analyse_wordlist(args.wordlist)
-                    
-    
-    ## indirect-hashtype-hashed-charact(default) or indirect-hashtype-hashed-charact-output
-    if args.mode == "indirect":
-        if args.type != None:
-            if args.hashed != None:
-                args.hashed = analyse_hashed(args.hashed)
-                if args.charact != None:
-                    args.charact = analyse_charact(args.wordlist)
-                    
-    ## Generate mode
-    if args.mode == "Gendict":
-        if args.type != None:
-            if args.hashed != None or args.hashed ==None:
-                args.hashed = None
-                if args.charact != None and args.wordlist == None:
-                    args.charact = analyse_charact(args.charact)
-                if args.charact == None and args.wordlist != None:
-                    args.wordlist = analyse_wordlist(args.wordlist)
-    """
-    # Create a generator for the wordlist
- words_to_hash = wordlist_generator('L?N', 3, 3, 'aeiou')
- 
- # Process each hashed word as it's generated
- for hashed_word in parallel_hashing(words_to_hash):
- print(hashed_word)
- # Include a condition to break the loop if necessary
-
-    """
-## Take a parameter determine whether the file is a wordlist or a path to multiple wordlists, it then extract all words from the list
-## I should optimize this part later on to make more memory friendly
-# as of now all the words are store in memory for processing    
-def analyse_wordlist(wordlist):
-    words = []
-    f = open(wordlist,"r").readlines()
-    size = f.__len__() - 1
-    for i in f:
-        a = i.strip()
-        if open(a):
-            print("hi")
-        else:
-            print("bye")
-    return 
-=======
-    ## indirect-hashtype-hashed-charact(default) or indirect-hashtype-hashed-charact-output
-    ## wordlist
-    ## hashed
-    ## charact
-    
+      
 ## Please don't provide a file of mixed path and words    
 def analyse_item(input_path, result=None):
     if result is None:
@@ -204,25 +113,140 @@ def sanitize(original_list):
 ## Function to output results
 def output(result,filepath):
     open(filepath, "r").append(result)
->>>>>>> Stashed changes
+
+## Sanitize inputs(This test the inputs and return error if they're unusable)
+def san_input(args):
+    gen_mode = False
+    wordlist = False
+    pattern = False
+    output = False
+    htype = None
+    if (args.mode == "Gendict" ):
+        if args.hashed != None:
+            print("1 You can't do that")
+        gen_mode = True
+            #exit()
+    if args.hashed == None and gen_mode == False:
+        print("2 You can't do that")
+    if args.wordlist != None and args.pattern != None:
+        print("3 Please check you're input first")
+        ## Ends here
+    if args.wordlist != None:
+      try: 
+           open(args.wordlist, "r")
+           wordlist = True
+      except:
+           print("4 Unable to use file")
+    if args.pattern != None:
+      for char in args.pattern:
+           if char not in default_characters:
+               print("5 Please refer to the documentation when using pattern")
+           else:
+               pattern = True
+    if args.output != None:
+      try: 
+         open(args.output, "w")
+         output = True
+      except:
+         print("6 Unable to use file")
+    if pattern == True:
+        
+        try:
+            if int(args.minlenght) > int(args.maxlenght):
+                print("7 lenght aren't consistent")
+        except:
+            print("7-1 excepting integer")
+        # End
+    if pattern == True:
+      while len(args.pattern)<int(args.minlenght):
+            args.pattern = args.pattern+"?"
+            print("8 Using default lenghts")
+    if args.type != None:
+        hash_type = args.type.lower() # Convert to lowercase for consistency
+        try:
+            getattr(hashlib, hash_type)()
+            htype = True
+        except AttributeError:
+            print(f"Invalid hash type: {hash_type}")
+    if args.type == None:
+        print("Please specifie the hash type to use")
+            
+    return gen_mode, wordlist, pattern, output, htype
+def st_crack(gene=False):
+    if gene == False:
+        for hashes in parallel_hashing(wordlist_read(sanitize(analyse_item(args.wordlist))), args.type):
+            try:
+                ## hashes, target_hash
+                a = compare_hash(hashes,args.hashed)
+                if a[0] == True:
+                    print(f"{a[1]} : {a[2]}")
+                    return None ## to stop all the multipocessing
+            except Exception as e:
+                print("",end="")
+    else:
+        for lenght in range(int(args.minlenght), int(args.maxlenght)+1):
+            if len(args.pattern)<lenght:
+                args.pattern = args.pattern+"?"
+                word = wordlist_generator(args.pattern,lenght,int(args.maxlenght))
+            else:
+                word = wordlist_generator(args.pattern,lenght,int(args.maxlenght))
+            for hashes in parallel_hashing(word, args.type):
+                try:
+                    ## hashes, target_hash
+                    a = compare_hash(hashes,args.hashed)
+                    if a[0] == True:
+                        print(f"{a[1]} : {a[2]}")
+                        return None ## to stop all the multipocessing
+                except Exception as e:
+                        print("",end="")
+                
+def genedict(gene=False):
+    if gene == False:
+        for hashes in parallel_hashing(wordlist_read(sanitize(analyse_item(args.wordlist))), args.type):
+            try:
+                print(f"{hashes[0]} : {hashes[1]}")
+            except Exception as e:
+                print("",end="")
+    else:
+        for lenght in range(int(args.minlenght), int(args.maxlenght)+1):
+            if len(args.pattern)<lenght:
+                args.pattern = args.pattern+"?"
+                word = wordlist_generator(args.pattern,lenght,int(args.maxlenght))
+            else:
+                word = wordlist_generator(args.pattern,lenght,int(args.maxlenght))
+            for hashes in parallel_hashing(word, args.type):
+                try:
+                    ## hashes, target_hash
+                    print(f"{hashes[0]} : {hashes[1]}")
+                except Exception as e:
+                        print("",end="")
+## Analyse input check errors launch other functions accordinaly
+def Manager(args):
+    #
+    tests = san_input(args)
+    print(tests)
+    if tests[0] == False:
+        if tests[1] == True:
+            st_crack()
+        else:
+            if tests[2] == True:
+                st_crack(gene=True)
+    else:
+         if tests[1] == True:
+             genedict()
+         else:
+            if tests[2] == True:
+                genedict(gene=True)
 ## Handle inputs
 #Initilise
 p = argparse.ArgumentParser()
 #argument list
 ## -c or --crack to define cracking process to use
-<<<<<<< Updated upstream
-p.add_argument('-m', '--mode', help="",default="direct")
-## The hash type or hashing algorithm to use.eg:md5, sha1
-p.add_argument('-t', '--type', help="", required=True)
-=======
-p.add_argument("-m",'--mode',choices=["direct","indirect","Gendict"], help="",default="direct")
+p.add_argument("-m",'--mode',choices=["direct","indirect","Gendict"], help="")
 ## The hash type or hashing algorithm to use.eg:md5, sha1
 p.add_argument('-t', '--type', help="")
->>>>>>> Stashed changes
 ## The hash that needs to be cracked
 p.add_argument('-hash', '--hashed', help="")
-## The set of characters to use
-p.add_argument('-char', '--charact', help="")
 ## The wordlist to use for the cracking process
 p.add_argument('-w','--wordlist', help="")
 ## The file were the result will be saved
@@ -231,6 +255,9 @@ p.add_argument('-o', '--output', help="")
 p.add_argument('-min','--minlenght', help="")
 ## maximun lenght
 p.add_argument('-max', '--maxlenght', help="")
+## pattern to use
+p.add_argument('-p', '--pattern', help="")
+##
 ## The verbose level
 
 #p.add_argument('-v','--verbose', help="Verbose", action="store_true")
@@ -239,34 +266,81 @@ args = p.parse_args()
 #logging.basicConfig(level=args.loglevel)
 
 #ver_inp(args)
-
+def hi():
+    print(args)
+    
 def main():
+    Manager(args)
     # banner()
     #analyse(args)
-<<<<<<< Updated upstream
-    analyse_wordlist("test.txt")
-    #logging.info("Message")
+    ### Hash a word
+    #print(hash_word("aT9", "md5"))
+    ### Word generator
+    #for word in wordlist_generator("L?", min_length=2, max_length=3):
+     #   print(word)
+    ### Word
     
-=======
-    ## Hashword
-    #print(hash_word("Hello", "md5"))
-    ## Generator wordlist
-    #for words in wordlist_generator():
-        #print(words)
-    ## Generator Files
-    #for words in wordlist_read(["test.txt"]):
-     #   print(words)
+    ### Hash Generetor2
+    """minl =5
+    maxl =10
+    for length in range(minl, maxl+1):
+        print(length)
+    """
+    """
+    save = open("start.txt", "a")
+    ## sanitize to remove duplicates, analyse_item to get all files
+    word = wordlist_generator()
+    for hashes in parallel_hashing(word, "md5"):
+        try:
+            print(hashes[0]) ## Hash
+            print(hashes[1]) ## Word
+        except:
+            print("1")
+            return None
+    """
+    ### Hash Generator1
+    """
+    save = open("start.txt", "a")
+    ## sanitize to remove duplicates, analyse_item to get all files
+    word = wordlist_read(sanitize(analyse_item("test.txt")))
+    for hashes in parallel_hashing(word, "md5"):
+        try:
+            print(hashes[0]) ## Hash
+            print(hashes[1]) ## Word
+        except:
+            print("1")
+    """
+    ### Generator, direct with good cracking
+    """
+    save = open("start", "a")
+    ## sanitize to remove duplicates, analyse_item to get all files
+    word = wordlist_read(sanitize(analyse_item("test.txt")))
+    for hashes in parallel_hashing(word, "md5"):
+        try:
+            ## hashes, target_hash
+            a = compare_hash(hashes,"16636a84a876abfbe4105bb63cbdeb07")
+            if a[0] == True:
+                print(a[1]) ## Hash
+                print(a[2]) ## Word
+                return None ## to stop all the multirpocessing
+        except:
+            print("1")
+     """
+    ### Generator, indirect with good cracking
+    """        
     save = open("start","a")
     word = wordlist_generator()
     for hashes in parallel_hashing(word, "md5"):
-        compare_hash(hashes,"16636a84a876abfbe4105bb63cbdeb07")
-    
-    """for hashes in parallel_hashing(words, "md5"):
-        save.writelines(hashes+"\n")
-    print(sanitize(analyse_item("test.txt")))
-    #logging.info("Message")
-    """
->>>>>>> Stashed changes
+        try:
+            ## hashes, target_hash
+            a = compare_hash(hashes,"16636a84a876abfbe4105bb63cbdeb07")
+            if a[0] == True:
+                print(a[1]) ## Hash
+                print(a[2]) ## Word
+                return None ## to stop all the multirpocessing
+        except:
+            print("1")
+     """
 
 if __name__=="__main__":
     main()
--- readme.md
+++ readme.md
@@ -1,18 +1,6 @@
-# HashWolf
+# HashCrack
 
-This project is a object oriented open source, Software for Windows/Linux made in Python 3 
-Hashwolf is an hashcracker design to be user friendly, easy to use, fast and lightweight, it currently have 2/4(direct and indirect) of it available modes(direct, indirect, dictionnary, rainbow)
-the other modes will be finished in a week or two after this version publish
-This project is lightweight and can be used to crack hash in environment with low storage space but high ram or medium
-Hashwolf(library) can be used in other program as it works independly from hashwolf(launcher or main), hashwolf(main) only provide the input needed for hashwolf(library) to work
-that's how it is represented:
-	`hashwolf.start(mode, hash_type, wordlist, hash_to_crack, char_to_use, output_file)`some input can be left undefined(None) those are char_to_use, output_file, wordlist(for indirect but must be defined for direct mode)
-
-## direct mode
-Direct mode of hashwolf is a mode in which hashwolf uses a wordlist provided by user to crack an hash (in ROM)
-## indirect mode
-indirect mode uses high amount of free ram, it generate a word in ram, hash it then compare instead of generating whole list in memory, which eventually will freeze the device
-It tries to use minimal ram
+This project is an object oriented open source, Software for Windows/Linux made in Python 3 
 
 ## Installation
 
@@ -20,34 +8,49 @@ You will need:
 
 * [Python 3.6+](https://www.python.org/downloads) (Make sure to add python to PATH during installation)
 * A Windows/Linux Computer
-* Can be run on terminal emulator such as termux...
-* Atleast 800 mb ram free if you intend to use indirect mode
-
-1. Download the repository via github or git eg. `git clone https://github.com/Xnetwolf/hashwolf`
-2. No need to install any requirements this project uses pre install modules
+1. Download the repository via github or git eg. `git clone https://github.com/rawbytedev/Hashcrack`
+2. install the required modules by running `python -m pip install -r requirements.txt`
 
 ## Features
 
-Currently this program crack those hash:
+#### Flexibility
 
-* md5
-* sha1
-* sha224
-* sha256
-* sha384
-* sha512
-* blake2b
-* blake2s
+*  Work with multiple wordlist at once
+- Work with multiple wordlist of hashes(in hexadecimal)
+
+- Custom patterns with lenght constraints 
+
+#### Object Oriented
+
+- Can be used in another project
+
+- Documentation of [HashCrack](https://github.com/HashCrack/docs/index.md) 
+
+#### Supported Hashes
+
+Support all the hashes provided by hashlib library
+
+#### Memory Friendly
+
+Most of the functions depends upon disk(speed) and cpu, the usage of memory is limited and is only used to stored data needed for processing. For memory comsumption process like generation of words, Generators is used instead of holding entire list in memory 
+
+- Optimizing Character Sets
+
+- Reducing Redundant Combinations
+
+- Pattern Matching
+
+#### MultiProcessing
+
+The use multiprocessing based hash generator increase efficiency
+
+Hashing is a task that depend hightly on the cpu that's why multiprocessing is used here, that way it uses all the cores availbale for the hashing process
+
+#### 
 
 ## Quick Usage
 
-1. use the following command:
-`python main.py -h` and follow the instructions on screen to start you're cracking 
-2. Don't forget to inclose the hash to crack between quotes `' '`('{hash}')
-
-example of usage:
-`python main.py -a indirect -t md5 -out cracked.txt -hash 'I\xf6\x8a\\\x84\x93\xec,\x0b\xf4\x89\x82\x1c!\xfc;' ` 
-you notice the absence of b and presence of quote(' hash ')
+1. Run `main.py` and follow the instructions on screen 
 
 ## Help
 
@@ -55,33 +58,19 @@ If you need any help at all, feel free to open a "help" issue.
 
 ## Error / Bugs
 
-If you find any error, feel free to let me know
-
-## Contribute to this project
-buy me a cup of coffee
-
-Bitcoin: `12iuxEDyQtGY2QgD3sHp3KrV363rcsBcMi`
-Dogecoin: `DPDuizKPUe8LodMGR9S8S388aKqnTbjWto`
-Litecoin: `LZqvfZYXdJr3D7nHQWi9KxW5hL6DaThbnE`
-BNB: `0x07A88F71AD5f95F592FFfd3D58306bED55706667`
-Polygon: `0x07A88F71AD5f95F592FFfd3D58306bED55706667`
-ETH: `0x07A88F71AD5f95F592FFfd3D58306bED55706667`
-DGB: `dgb1qvhuw7tddr7hat93365tkj4ee9v8yzd9ghjpe2w`
-TRX: `TTLvFFxpDyvvB7rvjVpYDQjeMzRqvwssXo`
-
-
+Please if you enconteer an error while using it, feel free to open an issue
 
 ## About
 
-Project Created by Rad Taren
-My repo:
-	https://github.com/Xnetwolf
+Project Created by rawbytedev(Aren)
+My repo: [rawbytedev](https://github.com/rawbytedev)
 
 ## Disclaimer
 
 This program is for educational purposes only! I take no responsibility or liability for own personal use.
 
-## Licence 
-copyright (c) Rad Taren. All rights reserved.
+## Licence
+
+copyright (c) Aren. All rights reserved.
 
 [license] under MIT(./license)
